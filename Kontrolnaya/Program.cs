740e655108d4e32db29baed044cf794ea111e456
Anton Popov <m15.tony27@gmail.com>
2022-05-14 18:18:43 +0400

    Код программы.

---
+++ Kontrolnaya/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] myArray = {"Hello", "My", "World"};
+string[] printArray = new string[myArray.Length];
+string k = "";
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i].Length <= 3) 
+    {
+        printArray[i] = myArray[i];          //Записываем элемент в массив другой массив.
+        Console.Write(printArray[i] + "\t"); //Печатаем элемент массива.
+        k = myArray[i];
+    }
+}
+
+if (k.Length == 0) Console.WriteLine("В массиве нет элементов с количеством символов <= 3.");
+return;
